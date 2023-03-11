0721673b2cf5505ec20f844673df8dbff4e27d19
CYTech Student <vincent.ky09@gmail.com>
2023-03-11 17:13:11 +0000

    changing Game function to work with new environement and agents

--- src/main/java/org/tovivi/environment/Game.java
+++ src/main/java/org/tovivi/environment/Game.java
@@ -2,11 +2,16 @@ package org.tovivi.environment;
 
 
 import org.tovivi.agent.Agent;
+import org.tovivi.agent.Legume;
 import org.tovivi.agent.RandomAgent;
+import org.tovivi.environment.action.Actions;
 
+import javax.swing.text.PlainDocument;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Game {
 
@@ -14,17 +19,85 @@ public class Game {
 
     private HashMap<String, Continent> continents = new HashMap<>();
     private HashMap<String, Tile> tiles = new HashMap<>();
-    private ArrayList<Agent> players = new ArrayList<>();
+    private HashMap<String, Agent> players = new HashMap<>();
 
     public Game() {
+        // number of troops per players
+        int troops = 35;
         // x players of less
-        players.add(new RandomAgent("Blue", this));
-        players.add(new RandomAgent("Red", this));
+        players.put("Blue", new RandomAgent("Blue", this));
+        players.put("Red", new RandomAgent("Red", this));
+        players.put("Grey", new Legume("Grey", this));
 
-        TextReader tr = new TextReader() ;
+        TextReader tr = new TextReader();
         tr.readAll(this, env_data);
 
+        if (troops < (int)(tiles.size()/players.size())) {
+            System.out.println("Not enough troops");
+            System.exit(-1);
+        }
+
+        // assign territories to players
+        // build a list made with tiles
+        ArrayList<Tile> tilesLeft = new ArrayList<>(tiles.values());
+        for(Agent a : players.values()) {
+            int localTroops = troops;
+            // assigning territories
+            for(int i=1; i <= (int)(tiles.size()/players.size()); i++) {
+                int index = (int)(Math.random() * tilesLeft.size());
+                tilesLeft.get(index).setOccupier(a, 1);
+                localTroops -= 1;
+                tilesLeft.remove(index);
+            }
+            // adding more troops
+            for(int i=1; i<=localTroops; i++) {
+                Tile tile = a.getTiles().get((int)(Math.random() * a.getTiles().size()));
+                tile.setNumTroops(tile.getNumTroops()+1);
+            }
+        }
+
         // for each player --> deploy, attack, fortify
+        int index = 0;
+        ArrayList<Agent> turns = new ArrayList<>(players.values());
+        while(turns.size() > 1) {
+            Agent p = turns.get(index);
+            Actions a = p.action();
+
+            System.out.println("Player " + p.getColor() + "'s turn");
+
+            // deploy
+            if (a.getDeployment().perform(p)) {
+                System.out.println("    [Success] :: " + a.getDeployment().toString());
+            } else {
+                System.out.println("    [Failed] :: " + a.getDeployment().toString());
+            }
+
+            // attack
+            if (a.getFirstOffensive().perform(p)) {
+                System.out.println("    [Success] :: " + a.getFirstOffensive().toString());
+            } else {
+                System.out.println("    [Failed] :: " + a.getFirstOffensive().toString());
+            }
+
+            System.out.println("    [TOTAL TERRITORIES : " + p.getTiles().size() + "]");
+
+            // check if players lose
+            Iterator<Agent> iterator = turns.iterator();
+            while (iterator.hasNext()) {
+                Agent agent = iterator.next();
+                if (agent.getTiles().size() == 0) {
+                    iterator.remove();
+                    System.out.println("[DEAD] The player " + agent.getColor() + " is DEAD like a beetroot !");
+                }
+            }
+
+            // next player
+            index += 1;
+            if (index >= turns.size()) {
+                index = 0;
+            }
+        }
+        System.out.println("[END] The winner is : " + turns.get(0).getColor() + ". Psartek !");
     }
 
     public HashMap<String, Continent> getContinents() {
@@ -41,6 +114,10 @@ public class Game {
 
     public HashMap<String, Tile> getTiles() {
         return tiles;
+    }
+
+    public HashMap<String, Agent> getPlayers() {
+        return players;
     }
 
     public static void main(String[] args) throws IOException {
