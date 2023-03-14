163c6586ce33a4f29ff4e08845bb59015bb2789d
DiosT-t <ttomtomy@gmail.com>
2023-03-14 16:40:54 +0000

    Deep Copy de game

--- src/main/java/org/tovivi/agent/AgentMonteCarlo.java
+++ src/main/java/org/tovivi/agent/AgentMonteCarlo.java
@@ -3,8 +3,10 @@ package org.tovivi.agent;
 import org.tovivi.environment.Game;
 import org.tovivi.environment.action.Actions;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class AgentMonteCarlo extends Agent {
-    public AgentMonteCarlo(String color, Game game) {
+    public AgentMonteCarlo(String color, Game game) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         super(color, game);
     }
 
